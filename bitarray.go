3017051cf7a7c9462790e9103ce7213fc271d365
Federico Ragona <federico.ragona@gmail.com>
2022-11-03 05:50:28 +0100

    Return error on invalid positions

--- bitarray.go
+++ bitarray.go
@@ -1,13 +1,14 @@
 package gloom
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
 
 type BitArray []uint64
 
-func NewBitArray(len int) BitArray {
+func NewBitArray(len uint64) BitArray {
 	l := len / 64
 	if len%64 > 0 {
 		l++
@@ -15,14 +16,23 @@ func NewBitArray(len int) BitArray {
 	return make(BitArray, l)
 }
 
-func (b BitArray) Flip(at int) {
+func (b BitArray) Flip(at int) error {
+	if at >= b.Len() {
+		return fmt.Errorf("invalid position: %v (out of %v)", at, len(b))
+	}
 	index, offset := at/64, at%64
 	b[index] |= 1 << offset
+
+	return nil
 }
 
-func (b BitArray) At(at int) bool {
+func (b BitArray) At(at int) (bool, error) {
+	if at >= b.Len() {
+		return false, fmt.Errorf("invalid position: %v (out of %v)", at, len(b))
+	}
+
 	index, offset := at/64, at%64
-	return b[index]&(1<<offset) != 0
+	return b[index]&(1<<offset) != 0, nil
 }
 
 func (b BitArray) String() string {
@@ -35,4 +45,8 @@ func (b BitArray) String() string {
 	}
 
 	return buf.String()
+}
+
+func (b BitArray) Len() int {
+	return len(b) * 64
 }
--- bitarray_test.go
+++ bitarray_test.go
@@ -8,9 +8,9 @@ import (
 func TestBitArray_NewBitArray_UpTo64(t *testing.T) {
 	b := NewBitArray(64)
 
-	b.Flip(63)
-	b.Flip(31)
-	b.Flip(0)
+	assert.NoError(t, b.Flip(63))
+	assert.NoError(t, b.Flip(31))
+	assert.NoError(t, b.Flip(0))
 
 	assert.Equal(t, "8000000080000001", b.String())
 }
@@ -18,10 +18,10 @@ func TestBitArray_NewBitArray_UpTo64(t *testing.T) {
 func TestBitArray_NewBitArray_MoreThan64(t *testing.T) {
 	b := NewBitArray(65)
 
-	b.Flip(64)
-	b.Flip(63)
-	b.Flip(31)
-	b.Flip(0)
+	assert.NoError(t, b.Flip(64))
+	assert.NoError(t, b.Flip(63))
+	assert.NoError(t, b.Flip(31))
+	assert.NoError(t, b.Flip(0))
 
 	assert.Equal(t, "8000000080000001-1", b.String())
 }
@@ -29,9 +29,9 @@ func TestBitArray_NewBitArray_MoreThan64(t *testing.T) {
 func TestBitArray_Flip(t *testing.T) {
 	b := NewBitArray(64)
 
-	b.Flip(63)
-	b.Flip(31)
-	b.Flip(0)
+	assert.NoError(t, b.Flip(63))
+	assert.NoError(t, b.Flip(31))
+	assert.NoError(t, b.Flip(0))
 
 	assert.Equal(t, "8000000080000001", b.String())
 }
@@ -39,9 +39,17 @@ func TestBitArray_Flip(t *testing.T) {
 func TestBitArray_At(t *testing.T) {
 	b := NewBitArray(64)
 
-	b.Flip(31)
+	assert.NoError(t, b.Flip(31))
 
-	assert.True(t, b.At(31))
-	assert.False(t, b.At(0))
-	assert.False(t, b.At(63))
+	if at, err := b.At(31); assert.NoError(t, err) {
+		assert.True(t, at)
+	}
+
+	if at, err := b.At(0); assert.NoError(t, err) {
+		assert.False(t, at)
+	}
+
+	if at, err := b.At(63); assert.NoError(t, err) {
+		assert.False(t, at)
+	}
 }
