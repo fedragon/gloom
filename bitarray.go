dca6c8c49007c73ade3e6ad0021c77c5a8f44373
Federico Ragona <federico.ragona@gmail.com>
2022-11-03 06:10:01 +0100

    Make public array length equal to requested one
    even though the underlying implementation might allocate a longer array
    to represent all bits

--- bitarray.go
+++ bitarray.go
@@ -6,47 +6,59 @@ import (
 	"strings"
 )
 
-type BitArray []uint64
+// BitArray represents an array of bits.
+type BitArray struct {
+	buckets []uint64
+	len     uint64
+}
 
-func NewBitArray(len uint64) BitArray {
+// NewBitArray returns a new instance with the requested length.
+func NewBitArray(len uint64) *BitArray {
 	l := len / 64
 	if len%64 > 0 {
 		l++
 	}
-	return make(BitArray, l)
+
+	return &BitArray{
+		buckets: make([]uint64, l),
+		len:     len,
+	}
 }
 
-func (b BitArray) Flip(at int) error {
+// Flip sets the bit at the `at` position to 1. It returns an error if `at` is greater or equal to the array's length.
+func (b *BitArray) Flip(at uint64) error {
 	if at >= b.Len() {
-		return fmt.Errorf("invalid position: %v (out of %v)", at, len(b))
+		return fmt.Errorf("invalid position: %v (out of %v)", at, b.Len())
 	}
 	index, offset := at/64, at%64
-	b[index] |= 1 << offset
+	b.buckets[index] |= 1 << offset
 
 	return nil
 }
 
-func (b BitArray) At(at int) (bool, error) {
+// At returns true if the bit at the `at` position is set to 1, false otherwise. It returns an error if `at` is greater or equal to the array's length.
+func (b *BitArray) At(at uint64) (bool, error) {
 	if at >= b.Len() {
-		return false, fmt.Errorf("invalid position: %v (out of %v)", at, len(b))
+		return false, fmt.Errorf("invalid position: %v (out of %v)", at, b.Len())
 	}
 
 	index, offset := at/64, at%64
-	return b[index]&(1<<offset) != 0, nil
+	return b.buckets[index]&(1<<offset) != 0, nil
 }
 
-func (b BitArray) String() string {
+// Len returns the lenght of the array.
+func (b *BitArray) Len() uint64 {
+	return b.len
+}
+
+func (b *BitArray) String() string {
 	var buf strings.Builder
-	for i, v := range b {
+	for i, v := range b.buckets {
 		buf.WriteString(strconv.FormatUint(v, 16))
-		if i < len(b)-1 {
+		if i < len(b.buckets)-1 {
 			buf.WriteString("-")
 		}
 	}
 
 	return buf.String()
-}
-
-func (b BitArray) Len() int {
-	return len(b) * 64
 }
--- bitarray_test.go
+++ bitarray_test.go
@@ -1,39 +1,35 @@
 package gloom
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBitArray_NewBitArray_UpTo64(t *testing.T) {
-	b := NewBitArray(64)
+	var capacity uint64 = 64
+	b := NewBitArray(capacity)
 
-	assert.NoError(t, b.Flip(63))
-	assert.NoError(t, b.Flip(31))
-	assert.NoError(t, b.Flip(0))
-
-	assert.Equal(t, "8000000080000001", b.String())
+	assert.Equal(t, capacity, b.Len())
 }
 
 func TestBitArray_NewBitArray_MoreThan64(t *testing.T) {
-	b := NewBitArray(65)
+	var capacity uint64 = 65
+	b := NewBitArray(capacity)
 
-	assert.NoError(t, b.Flip(64))
-	assert.NoError(t, b.Flip(63))
-	assert.NoError(t, b.Flip(31))
-	assert.NoError(t, b.Flip(0))
-
-	assert.Equal(t, "8000000080000001-1", b.String())
+	assert.Equal(t, capacity, b.Len())
 }
 
 func TestBitArray_Flip(t *testing.T) {
-	b := NewBitArray(64)
+	b := NewBitArray(244)
 
-	assert.NoError(t, b.Flip(63))
+	assert.NoError(t, b.Flip(243))
+	assert.NoError(t, b.Flip(121))
+	assert.NoError(t, b.Flip(66))
 	assert.NoError(t, b.Flip(31))
 	assert.NoError(t, b.Flip(0))
 
-	assert.Equal(t, "8000000080000001", b.String())
+	assert.Equal(t, "80000001-200000000000004-0-8000000000000", b.String())
 }
 
 func TestBitArray_At(t *testing.T) {
